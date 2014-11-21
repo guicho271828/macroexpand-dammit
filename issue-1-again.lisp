3b6013f0f8d54d12cd14df2c331d82b793553fa7
DalekBaldwin <DalekBaldwin@users.noreply.github.com>
2014-11-21 01:21:57 -0800

    change handling of  to only impose bindings when a symbol-macro is already active

--- issue-1-again.lisp
+++ issue-1-again.lisp
@@ -2,8 +2,13 @@
 (in-package :macroexpand-dammit-test)
 (in-suite :macroexpand-dammit-test)
 
+#-allegro
+(declaim (type integer *declared-integer*))
+#+allegro
+(eval-when (:load-toplevel :execute)
+  (proclaim '(type integer *declared-integer*)))
 
-
+(defvar *declared-integer* 0)
 (defvar *compile-time-counter* 0)
 
 (defmacro count-up ()
@@ -66,3 +71,23 @@
            (compiled-fn (compile nil `(lambda () ,expanded))))
       (is (= 4 (funcall compiled-fn)))
       (is (= 2 *compile-time-counter*)))))
+
+(test issue1-type-declaration
+  ;; Ensure that in the absence of a symbol-macro binding, no dummy binding is
+  ;; imposed that would conflict with the type declaration for a global
+  ;; variable.
+  (is (=
+       5
+       (eval
+        (macroexpand-dammit
+         '(symbol-macrolet ((not-a-special-variable 1))
+           (let ((*declared-integer* 2)
+                 (not-a-special-variable 3))
+             (+ *declared-integer* not-a-special-variable)))))))
+  
+  ;; In ANSI CL it is an error to establish a symbol-macro binding for an
+  ;; existing global variable name, so no let binding for such a variable can
+  ;; ever shadow a symbol-macro anyway.
+  (signals error
+    (macroexpand-dammit
+     '(symbol-macrolet ((*declared-integer* 'foo))))))
